9ad69e177b1855eb8d3b72803d5eb70c36f11168
bryx2602 <118649147+bryx2602@users.noreply.github.com>
2023-04-10 09:56:43 +0800

    Update db_conn.php

--- db_conn.php
+++ db_conn.php
@@ -6,7 +6,7 @@ $password = "";
 
 $db_name = "test_db";
 
-$conn = mysqli_connect($sname, $unmae, $password, $db_name);
+$conn = mysqli_connect($sname, $uname, $password, $db_name);
 
 if (!$conn) {
 	echo "Connection failed!";
